f19de81fd3db0ec64033fb775db2f1b134216cd0
bigBettle <2211915633@qq.com>
2022-05-02 11:28:14 +0800

    初始化

---
+++ pages/government/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--  政府人员页面-->
+  <!--  在这里写html,vue前端代码-->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //  数据
+    }
+  },
+
+  methods: {
+    //  编写方法
+  }
+}
+
+</script>
+<style scoped>
+/*css 代码*/
+</style>
